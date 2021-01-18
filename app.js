48b85bee460eec039742e61d03fd6fd363dfe4ca
Rafael De Hermoso <rdehermoso@hotmail.com>
2021-01-17 19:01:00 -0500

    Loggin db errors

--- app.js
+++ app.js
@@ -22,4 +22,10 @@ mongoose.connect(
     () => console.log('connected to DB!')
 );
 
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 app.listen(3000);
